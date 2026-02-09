0a1396ff7e3d37498a672a08461269954c9b6b3b
Fikriwib12 <wibfikri@gmail.com>
2026-02-09 23:07:54 +0700

    fix error ipmort login

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router";
-import Login from "./pages/login.jsx";
+import Login from "./pages/Login.jsx";
 import Home from "./pages/Home.jsx";
 
 const router = createBrowserRouter([
